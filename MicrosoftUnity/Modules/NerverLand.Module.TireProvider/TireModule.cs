4d9a0f19d28e41d0f16f476482d2422286148eca
Toan Le - Never land <toan.le.group@gmail.com>
2013-11-19 09:02:58 -0800

    * trunk/MicrosoftUnity/MicrosoftUnity.sln * trunk/MicrosoftUnity/Modules/NerverLand.Module.ChassisProvider/ChassisModule.cs * trunk/MicrosoftUnity/Modules/NerverLand.Module.ChassisProvider/ChassisProvider.cs * trunk/MicrosoftUnity/Modules/NerverLand.Module.ChassisProvider/NerverLand.Module.ChassisProvider.csproj * trunk/MicrosoftUnity/Modules/NerverLand.Module.ChassisProvider/Properties/AssemblyInfo.cs * trunk/MicrosoftUnity/Modules/NerverLand.Module.EngineProvider/EngineModule.cs * trunk/MicrosoftUnity/Modules/NerverLand.Module.EngineProvider/EngineProvider.cs * trunk/MicrosoftUnity/Modules/NerverLand.Module.EngineProvider/NerverLand.Module.EngineProvider.csproj * trunk/MicrosoftUnity/Modules/NerverLand.Module.EngineProvider/Properties/AssemblyInfo.cs * trunk/MicrosoftUnity/Modules/NerverLand.Module.TireProvider/NerverLand.Module.TireProvider.csproj * trunk/MicrosoftUnity/Modules/NerverLand.Module.TireProvider/Properties/AssemblyInfo.cs * trunk/MicrosoftUnity/Modules/NerverLand.Module.TireProvider/TireModule.cs * trunk/MicrosoftUnity/Modules/NerverLand.Module.TireProvider/TireProvider.cs

---
+++ MicrosoftUnity/Modules/NerverLand.Module.TireProvider/TireModule.cs
@@ -0,0 +1,11 @@
+﻿
+using System.ComponentModel.Composition;
+using NerverLand.Module.Contract.Modules;
+
+namespace NerverLand.Module.TireProvider
+{
+    [Export(typeof(IModule))]
+    public class TireModule : TireModuleBase.TireModuleBase
+    {
+    }
+}
---
+++ MicrosoftUnity/Modules/NerverLand.Module.TireProvider/TireProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NerverLand.Module.Contract.Provider;
+
+namespace NerverLand.Module.TireProvider
+{
+    public class TireProvider : ITireProvider
+    {
+        public string GetTire(int size)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
