632f5f236a752a2d41f915e217265ba1aaff3e85
Toan Le - Never land <toan.le.group@gmail.com>
2013-11-22 00:01:46 -0800

    Config Autofac and Unity in the same project

---
+++ ModularApplicationDemo/NerverLand.WebApp/Models/CarModel.cs
@@ -0,0 +1,22 @@
+﻿namespace NerverLand.WebApp.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CarModel
+    {
+        [Display(Name="Car Name")]
+        public string CarName { get; set; }
+
+        [Display(Name = "Model Year")]
+        public int Year { get; set; }
+
+        [Display(Name = "Tire Size")]
+        public string TireModel { get; set; }
+
+        [Display(Name = "Chassis Model")]
+        public string ChassisModel { get; set; }
+
+        [Display(Name = "Engine Info")]
+        public string EngineModel { get; set; }
+    }
+}
