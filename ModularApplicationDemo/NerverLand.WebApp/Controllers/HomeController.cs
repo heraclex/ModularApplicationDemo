2023cbe2baffb031bb16b909c53beed7be8ab36a
Toan Le - Never land <toan.le.group@gmail.com>
2013-12-02 20:48:00 -0800

    update Home Pages and remove redundant views

--- ModularApplicationDemo/NerverLand.WebApp/Controllers/HomeController.cs
+++ ModularApplicationDemo/NerverLand.WebApp/Controllers/HomeController.cs
@@ -6,13 +6,35 @@ using System.Web.Mvc;
 
 namespace NerverLand.WebApp.Controllers
 {
+    using NerverLand.CarComponent.Contract;
+    using NerverLand.WebApp.Models;
+
     public class HomeController : Controller
     {
+        private ITireProvider tireProvider;
+
+        private IChassisProvider chassisProvider;
+
+        private IEngineProvider engineProvider;
+
+        public HomeController(ITireProvider tireProvider, IChassisProvider chassisProvider, IEngineProvider engineProvider)
+        {
+            this.tireProvider = tireProvider;
+            this.chassisProvider = chassisProvider;
+            this.engineProvider = engineProvider;
+        }
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
+            var model = new CarModel
+            {
+                CarName = "New Car model 2013: producted by factory component",
+                ChassisModel = this.chassisProvider.GetComponentName(),
+                EngineModel = this.engineProvider.GetComponentName(),
+                TireModel = this.tireProvider.GetComponentName(),
+                Year = 2013
+            };
+            return this.View(model);
         }
 
         public ActionResult About()
